b793e59e0fddbc8fa932057071717c313dc68d3a
Subhra Swarup Behera <subhra.behera@tavant.com>
2019-01-28 16:48:18 +0530

    new code added

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace DatingApp.API
 services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));  
             //services.AddDbContext<DataContext>(x=>x.UseSqlServer(Configuration.GetConnectionString("DefultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,7 @@ services.AddDbContext<DataContext>(options => options.UseSqlServer(connection)
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
