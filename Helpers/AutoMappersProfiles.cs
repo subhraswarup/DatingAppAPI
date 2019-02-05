3efdc8b1f07ddf8bf29b41237044d22dae43e048
subhra behera <subhraswarup@gmail.com>
2019-02-05 23:56:24 +0530

    End of section 10

--- Helpers/AutoMappersProfiles.cs
+++ Helpers/AutoMappersProfiles.cs
@@ -25,6 +25,7 @@ namespace DatingAppAPI.Helpers
                 opt.MapFrom(d=>d.DateOfBirth.CalculateAge());
             });
             CreateMap<Photo,PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
